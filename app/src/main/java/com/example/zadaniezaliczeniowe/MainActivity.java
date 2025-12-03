0a4a1ed441637d272f02b8027f150d8bb836e0b2
Szymon <szymko12@interia.pl>
2025-12-03 13:50:37 +0100

    third commit

--- app/src/main/java/com/example/zadaniezaliczeniowe/MainActivity.java
+++ app/src/main/java/com/example/zadaniezaliczeniowe/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.zadaniezaliczeniowe;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editImie, editNazwisko, editDlugoscHasla;
+    Spinner spinnerStanowisko;
+    CheckBox checkLitery, checkCyfry, checkZnaki;
+    Button buttonGeneruj, buttonZatwierdz;
+
+    String wygenerowaneHaslo="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editImie =findViewById(R.id.editTextImie);
+        editNazwisko =findViewById(R.id.editTextNazwisko);
+        editDlugoscHasla =findViewById(R.id.editTextDlugoscHasla);
+        spinnerStanowisko =findViewById(R.id.spinnerStanowisko);
+        checkCyfry =findViewById(R.id.checkBoxCyfry);
+        checkLitery =findViewById(R.id.checkBoxLitery);
+        checkZnaki =findViewById(R.id.checkBoxZnaki);
+        buttonGeneruj =findViewById(R.id.buttonGeneruj);
+        buttonZatwierdz =findViewById(R.id.buttonZatwierdz);
+
+        String[] stanowisko = {"Kierownik", "Starszy Programista","Tester", "Mlodszy Programista",};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, stanowisko);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerStanowisko.setAdapter(adapter);
+
     }
 }
